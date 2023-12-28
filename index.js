551a07aaf2434a68459bc901b16690f85a109c09
Aisha <kalimatovaaisha@mail.ru>
2023-12-28 20:52:07 +0600

    init project

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const { Pool } = require('pg');
+const pool = new Pool({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "Aisha14zhas",
+    database: "backauth"
+});
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route: Default route, serves the main HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Route: Login page
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/pages/login.html'));
+});
+
+// Route: Handle login POST request
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const client = await pool.connect();
+    try {
+        const result = await client.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password]);
+        const fullname = await client.query('SELECT fullname FROM users WHERE username = $1', [username]);
+        const fullnameValue = fullname.rows[0].fullname;
+
+        if (result.rows.length > 0) {
+            res.json({ success: true, message: `Welcome, ${fullnameValue}!` });
+        } else {
+            res.json({ success: false, message: 'Invalid username or password.' });
+        }
+    } catch (error) {
+        console.error('Error executing query:', error);
+        res.status(500).json({ success: false, message: 'Internal server error.' });
+    } finally {
+        client.release(); // Release the connection back to the pool
+    }
+});
+
+// Route: Register page
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/pages/register.html'));
+});
+
+// Route: Handle registration POST request
+app.post('/register', async (req, res) => {
+    const { username, fullname, email, password } = req.body;
+
+    // Insert user registration data into the database
+    const client = await pool.connect();
+
+    try {
+        await client.query('INSERT INTO users (username, fullname, email, password) VALUES ($1, $2, $3, $4)', [username, fullname, email, password]);
+        res.status(200).json({
+            "status": "success",
+            "message": `You have successfully registered an account.`
+        });
+    } catch (error) {
+        console.error('Error executing query:', error);
+        res.status(500).json({
+            "success": false,
+            "message": "Something went wrong. Please try again."
+        });
+    } finally {
+        client.release();
+    }
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
